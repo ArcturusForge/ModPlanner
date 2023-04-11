807829f96e42cb28fbf61b97c29aa339e06a1223
Arcturus <arcturusforge@gmail.com>
2023-04-11 14:51:01 +1000

    Update Globals.gd

--- Scripts/Globals/Globals.gd
+++ Scripts/Globals/Globals.gd
@@ -1,7 +1,7 @@
 extends Node
 
 #-- Control
-const versionId = "3.4.6"
+const versionId = "3.5.0"
 const appName = "Mod Planner"
 const sessionNameDefault = "Untitled_Session"
 const saveExtension = "mplan"
